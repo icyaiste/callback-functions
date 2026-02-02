08ace0e40857c2d0ca2698190422ca40fb18e117
icy <icy.aiste@gmail.com>
2026-02-02 18:37:29 +0100

    Challenges 4-6 done

--- callback-challenges/challenges.ts
+++ callback-challenges/challenges.ts
@@ -33,3 +33,60 @@ function map(numbers: number[], callback: (number: number) => number): number[] 
     return result;
 }
 console.log(map([1, 2, 3], addTwo));
+
+// Challenge 4
+/* The function forEach takes an array and a callback, and runs the callback on each element of the array. 
+forEach does not return anything.*/
+
+let alphabet = "";
+const letters = ["a", "b", "c", "d"];
+
+function forEach <T>(array: T[],callback: (item : T) => void): void {
+    for (const item of array) {
+        callback(item);
+    }
+}
+
+forEach(letters, function (character) {
+  alphabet += character;
+});
+console.log(alphabet);
+// should output abcd
+
+// Challenge 5
+/* Rebuild your map function, this time instead of using a for loop, use your own forEach function that you just defined. 
+Call this new function mapWith.//console.log(mapWith([1, 2, 3], addTwo));*/
+
+function mapWith(numbers: number[], callback: (number: number) => number): number[] {
+    const result: number[] = [];
+
+    forEach(numbers, function(number) {
+        result.push(callback(number));
+    });
+    return result;
+}
+
+console.log(mapWith([1, 2, 3], addTwo)); 
+//should output [ 3, 4, 5 ]
+
+// Challenge 6
+/* 
+The function reduce takes an array and reduces the elements to a single value. 
+For example it can sum all the numbers, multiply them, 
+or any operation that you can put into a function.
+*/
+function reduce<T>(array : T[], callback : (accumulator : T, current : T) => T, initialValue : T) : T {
+    let accumulator = initialValue;
+    
+    forEach(array, function(item) {
+        accumulator = callback(accumulator, item);
+    });
+    return accumulator;
+}
+
+const nums = [4, 1, 3];
+const add = function (a: number, b: number): number {
+  return a + b;
+};
+console.log(reduce(nums, add, 0))
+//should output 8
