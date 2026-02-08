bc3fbe37e32effc7b482a8537f84f2925f74e1a7
icy <icy.aiste@gmail.com>
2026-02-08 17:34:10 +0100

    async await first exercise

---
+++ promises/asyncAwait/asyncAwait.ts
@@ -0,0 +1,22 @@
+const passwordLength = new Promise<string>((resolve, reject) => {
+  const password = "mySecretPassword56413485";
+
+if (password.length > 8) {
+    resolve("Strong password. Welcome aboard!");
+  } else {
+    reject(new Error("Password too weak! Try again"));
+  }
+});
+
+const handleSignIn = async (): Promise<void> => {
+    try {
+        const message = await passwordLength;
+        console.log(message);
+        //fetchAfterSignIn();
+    } catch (error) {
+        if(error instanceof Error) {
+            console.error(error.message);
+        }
+    }
+};
+handleSignIn();
